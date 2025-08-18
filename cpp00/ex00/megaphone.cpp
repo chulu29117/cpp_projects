30170573328048158d3ab816879119c0d67e1c23
chuchulu29117 <chuchulu117@gmail.com>
2025-08-18 17:04:30 +0300

    files added

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 16:22:16 by clu               #+#    #+#             */
+/*   Updated: 2025/08/18 17:04:12 by clu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		
+	}
+}
+
